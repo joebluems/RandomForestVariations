8b0aa0969c4f9083289d316c3a166b8d44ca05be
joebluems <joeblue@Josephs-MacBook-Pro.local>
2019-04-29 17:51:52 -0700

    moved images

--- 3_spark/build.sbt
+++ 3_spark/build.sbt
@@ -1,6 +1,6 @@
 name := "RandomForest"
-scalaVersion := "2.11.12"
-val sparkVersion = "2.4.0"
+scalaVersion := "2.11.8"
+val sparkVersion = "2.3.2"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
--- 4_sparklyr/sparklyR.R
+++ 4_sparklyr/sparklyR.R
@@ -11,6 +11,8 @@ library(purrr)
 ### Note: you will need to specify file location
 sc <- spark_connect(master = "local[*]")
 data_tbl <- spark_read_csv(sc, name = "data", path = "../sample10k.csv", header = TRUE, infer_schema = TRUE, delimiter = ",")
+print("Importing data...")
+head(data_tbl)
 
 ## create partitions table references
 partition <- data_tbl %>% 
@@ -18,12 +20,18 @@ partition <- data_tbl %>%
   sdf_partition(train = 0.6, test = 0.4, seed = 542)
 train_tbl <- partition$train
 test_tbl <- partition$test
+print("Dimensions of training and testing splits:")
+dim(train_tbl)
+dim(test_tbl)
 
 ############################################
 ###### 2. TRAIN, INTERPRET MODELS ##########
 ############################################
 ml_formula <- formula(target ~ f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10 + f11 + f12)
 ml_rf <- train_tbl %>% ml_random_forest(ml_formula, num_trees = 500, max_depth =5, type="classification")
+print("Model Trained:")
+print(ml_rf)
+summary(ml_rf)
 
 ### feature importance for random forest
 feature_importance <- tibble()
@@ -37,5 +45,6 @@ feature_importance
 ####### 3. EVALUATE MODEL (AUC) ##########
 ##########################################
 pred <- ml_predict(ml_rf, test_tbl) 
+print("AUC for test data set:")
 ml_binary_classification_evaluator(pred,metric_name = "areaUnderROC")
 
